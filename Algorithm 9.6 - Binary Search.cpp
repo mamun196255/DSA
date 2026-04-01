e254bcf2a672431b2e0f17e3f974386d6311d63d
Md Mamunur Rashid <mdmamunur169467@gmail.com>
2026-04-01 22:59:12 +0600

    Implement binary search algorithm in C++

---
+++ Algorithm 9.6 - Binary Search.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int A[] = {17, 19, 28, 30, 45, 55, 58, 61, 63, 67, 72, 76, 80, 89, 99};
+    int n = 15;
+    int item = 89;
+    int location = -1;
+
+    int first = 0;
+    int last = n - 1;
+
+    while (first <= last) {
+        int mid = (first + last) / 2;
+
+        if (A[mid] == item) {
+            location = mid;
+            break;
+        } else if (item < A[mid]) {
+            last = mid - 1;
+        } else {
+            first = mid + 1;
+        }
+    }
+
+    if (location != -1) {
+        cout << "Item " << item << " found at index " << location << "." << endl;
+    } else {
+        cout << "Item " << item << " not found." << endl;
+    }
+
+    return 0;
+}
